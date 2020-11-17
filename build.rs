2fa74b778c197e5938ce037d21ac3b4baf443165
Ian Graham Martinez <Ian.graham28@gmail.com>
2020-11-17 18:25:41 -0500

    Made shader compilation optional

--- build.rs
+++ build.rs
@@ -1,67 +1,69 @@
-use shaderc;
-use std::io::prelude::*;
+fn main() {
+	#[cfg(feature = "compile-shaders")]
+	{
+		use shaderc;
+		use std::io::prelude::*;
 
-fn main() -> std::io::Result<()> {
+		let mut compiler = shaderc::Compiler::new().unwrap();
 
-	let mut compiler = shaderc::Compiler::new().unwrap();
+		let mut options = shaderc::CompileOptions::new().unwrap();
 
-	let mut options = shaderc::CompileOptions::new().unwrap();
+		options.set_warnings_as_errors();
 
-	options.set_warnings_as_errors();
+		options.set_optimization_level(shaderc::OptimizationLevel::Performance);
 
-	options.set_optimization_level(shaderc::OptimizationLevel::Performance);
+		options.set_include_callback(|source_req, _, path, _| {
 
-	options.set_include_callback(|source_req, _, path, _| {
+			let path = std::path::Path::new(path);
 
-		let path = std::path::Path::new(path);
+			let full_req = format!("{}/{}", path.parent().unwrap_or(std::path::Path::new(".")).display(), source_req);
 
-		let full_req = format!("{}/{}", path.parent().unwrap_or(std::path::Path::new(".")).display(), source_req);
+			let mut content = String::new();
 
-		let mut content = String::new();
+			std::fs::File::open(std::path::Path::new(&full_req)).unwrap().read_to_string(&mut content).unwrap();
 
-		std::fs::File::open(std::path::Path::new(&full_req)).unwrap().read_to_string(&mut content).unwrap();
+			Ok(shaderc::ResolvedInclude {
+				resolved_name : full_req,
+				content,
+			})
 
-		Ok(shaderc::ResolvedInclude {
-			resolved_name : full_req,
-			content,
-		})
+		});
 
-	});
+		for maybe_file in std::fs::read_dir("assets/shaders").unwrap().into_iter().chain(std::fs::read_dir("src/reng/shaders").unwrap().into_iter()) {
 
-	for maybe_file in std::fs::read_dir("assets/shaders").unwrap().into_iter().chain(std::fs::read_dir("src/reng/shaders").unwrap().into_iter()) {
+			let file = maybe_file.unwrap();
 
-		let file = maybe_file.unwrap();
+			let pathbuf = file.path();
 
-		let pathbuf = file.path();
+			let path = pathbuf.as_path();
 
-		let path = pathbuf.as_path();
+			let extension = path.extension().unwrap().to_str().unwrap();
 
-		let extension = path.extension().unwrap().to_str().unwrap();
+			let kind = {
+				if extension == "vert"
+				{
+					shaderc::ShaderKind::Vertex
+				} else if extension == "frag" {
+					shaderc::ShaderKind::Fragment
+				} else if extension == "comp"{
+					shaderc::ShaderKind::Compute
+				} else {
+					continue
+				}
+			};
 
-		let kind = {
-			if extension == "vert"
-			{
-				shaderc::ShaderKind::Vertex
-			} else if extension == "frag" {
-				shaderc::ShaderKind::Fragment
-			} else if extension == "comp"{
-				shaderc::ShaderKind::Compute
-			} else {
-				continue
-			}
-		};
+			let mut new_shader_name = String::from(path.to_str().unwrap());
+			new_shader_name.push_str(".spv");
 
-		let mut new_shader_name = String::from(path.to_str().unwrap());
-		new_shader_name.push_str(".spv");
+			let mut new_shader = std::fs::File::create(new_shader_name).unwrap();
 
-		let mut new_shader = std::fs::File::create(new_shader_name).unwrap();
+			let source = std::fs::read_to_string(path).expect("Unable to read file");
 
-		let source = std::fs::read_to_string(path)?;
+			let spirv = compiler.compile_into_spirv(source.as_str(), kind, path.to_str().unwrap(), "main", Some(&options)).map_err(|e| println!("{}", e)).expect("");
 
-		let spirv = compiler.compile_into_spirv(source.as_str(), kind, path.to_str().unwrap(), "main", Some(&options)).map_err(|e| println!("{}", e)).expect("");
+			new_shader.write(spirv.as_binary_u8()).unwrap();
 
-		new_shader.write(spirv.as_binary_u8()).unwrap();
-
+		}
 	}
-	Ok(())
 }
+
